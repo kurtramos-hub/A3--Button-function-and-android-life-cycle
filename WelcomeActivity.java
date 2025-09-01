2c7bed6ec65935eef9c815c88aa5ed6e50ac6c0f
Kurt.28 <kurt.ramos@hcdc.edu.ph>
2025-09-01 09:48:12 +0800

    WelcomeActivity.java

---
+++ WelcomeActivity.java
@@ -0,0 +1,12 @@
+package com.example.app1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+    }
+}
